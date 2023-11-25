7872d7448717b704b0d1284e3265ccad6c42290a
Tom Smith <ThomUK@users.noreply.github.com>
2023-11-25 21:33:33 +0000

    run the model and output a plot

--- R/mod_simulation.R
+++ R/mod_simulation.R
@@ -10,9 +10,9 @@
 mod_simulation_ui <- function(id){
   ns <- NS(id)
   tagList(
-    selectInput(NS(id, "var"), "Variable", names(mtcars)),
-    numericInput(NS(id, "bins"), "bins", 10, min = 1),
-    plotOutput(NS(id, "hist"))
+    actionButton(NS(id, "updateButton"), "Update Model!", class = "btn-success"),
+
+    plotOutput(NS(id, "plot"))
   )
 }
 
@@ -23,18 +23,21 @@ mod_simulation_server <- function(id){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
 
-    data <- reactive(mtcars[[input$var]])
-    output$hist <- renderPlot({
-      hist(data(), breaks = input$bins, main = input$var)
-    }, res = 96)
+    observeEvent(input$updateButton, {
 
-      # sim <- run_sim()
-      #
-      # sim_resources <- sim |>
-      #   get_mon_resources()
-      #
-      # output$plot <- plot(sim_resources, metric = "usage", items = c("server", "queue"), steps = FALSE) +
-      #   scale_x_continuous(name = "days", labels = scales::number_format(scale = 1/60/24))  # format labels to represent days
+      # run the simulation
+      sim <- run_sim()
+
+      # compute some stats
+      sim_resources <- sim |>
+        get_mon_resources()
+
+      # make a plot
+      output$plot <- renderPlot(
+        plot(sim_resources, metric = "usage", items = c("server", "queue"), steps = TRUE) +
+          scale_x_continuous(name = "days", labels = scales::number_format(scale = 1/60/24))  # format labels to represent days
+      )
+    })
 
   })
 }
