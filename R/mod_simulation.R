c9cc87700528ea429dd48dedc68dd0920825f3f9
Tom Smith <ThomUK@users.noreply.github.com>
2023-11-26 22:21:38 +0000

    slider layout improvements

--- R/mod_simulation.R
+++ R/mod_simulation.R
@@ -24,32 +24,39 @@ mod_simulation_ui <- function(id){
           <li>The OP clinic and Theatre schedules are hard-coded as 8hrs/day, 7days/week.  This needs to be user-configurable.</li>
         </ul>"
     ),
-    column(
-      width = 6,
-      div(
-        style = "border: 2px solid #ddd; border-radius: 5px; padding: 10px;",
-        sliderInput(NS(id, "numForecastLength"), "Future period to forecast (weeks)", value = 1, min = 1, max = 104),
-        sliderInput(NS(id, "numPatReferralRate"), "Number of new patient referrals (monthly)", value = 100, min = 0, max = 1000),
-        sliderInput(NS(id, "numPatBacklogSize"), "Number of existing patients in the OP clinic backlog", value = 500, min = 0, max = 5000),
-        sliderInput(NS(id, "numAdmitConversionRate"), "Outpatient conversion rate (OP -> admission / treatment waiting list)", value = 0.1, min = 0, max = 1),
-        sliderInput(NS(id, "numFupRate"), "Outpatient followup rate", value = 0.25, min = 0, max = 1), #TODO check and improve this logic
+    fluidRow(
+      column(
+        width = 6,
+        h4("Model options:"),
+        div(
+          style = "border: 2px solid #ddd; border-radius: 5px; padding: 10px;",
+          sliderInput(NS(id, "numForecastLength"), "Future period to forecast (weeks)", value = 1, min = 1, max = 104),
+        )
       ),
+      column(
+        width = 6,
+        h4("Service config:"),
+        div(
+          style = "border: 2px solid #ddd; border-radius: 5px; padding: 10px; margin-top: 5px;",
+          sliderInput(NS(id, "numOpClinicLength"), "Length of an OP clinic (minutes)", value = 30, min = 5, max = 120),
+          numericInput(NS(id, "numBeds"), "Total beds (pre & post-operative combined)", value = 6, min = 1, max = 36),
+        )
+      )
     ),
-    column(
-      width = 6,
-      div(
-        style = "border: 2px solid #ddd; border-radius: 5px; padding: 10px; margin-top: 5px;",
-        sliderInput(NS(id, "numOpClinicLength"), "Length of an OP clinic (minutes)", value = 30, min = 5, max = 120),
-      ),
-      div(
-        style = "border: 2px solid #ddd; border-radius: 5px; padding: 10px; margin-top: 5px;",
-        numericInput(NS(id, "numBeds"), "Total beds (pre & post-operative combined)", value = 6, min = 1, max = 36),
-        sliderInput(NS(id, "numPreOpLos"), "Average pre-operative length of stay (hours)", value = 6, min = 0, max = 36),
-        sliderInput(NS(id, "numPostOpLos"), "Average post-operative length of stay (hours)", value = 36, min = 1, max = 96),
-      ),
-      div(
-        style = "border: 2px solid #ddd; border-radius: 5px; padding: 10px; margin-top: 5px;",
-        sliderInput(NS(id, "numTheatreProcLength"), "Average length of theatre procedures (minutes)", value = 90, min = 5, max = 720),
+    fluidRow(
+      column(
+        width = 6,
+        h4("Patient pathway config:"),
+        div(
+          style = "border: 2px solid #ddd; border-radius: 5px; padding: 10px;",
+          sliderInput(NS(id, "numPatBacklogSize"), "Number of existing patients in the OP clinic backlog", value = 500, min = 0, max = 5000),
+          sliderInput(NS(id, "numPatReferralRate"), "Number of new patient referrals (monthly)", value = 100, min = 0, max = 1000),
+          sliderInput(NS(id, "numFupRate"), "Outpatient followup rate", value = 0.25, min = 0, max = 1), #TODO check and improve this logic
+          sliderInput(NS(id, "numAdmitConversionRate"), "Outpatient conversion rate (OP -> admission / treatment waiting list)", value = 0.1, min = 0, max = 1),
+          sliderInput(NS(id, "numPreOpLos"), "Average pre-operative length of stay (hours)", value = 6, min = 0, max = 36),
+          sliderInput(NS(id, "numPostOpLos"), "Average post-operative length of stay (hours)", value = 36, min = 1, max = 96),
+          sliderInput(NS(id, "numTheatreProcLength"), "Average length of theatre procedures (minutes)", value = 90, min = 5, max = 720),
+        )
       )
     ),
     column(
