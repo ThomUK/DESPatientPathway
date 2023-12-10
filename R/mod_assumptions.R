6bb9825a5715bf98132c5c84232825a821ec6208
Tom Smith <ThomUK@users.noreply.github.com>
2023-12-10 23:13:33 +0000

    improve notes and assumptions page

--- R/mod_assumptions.R
+++ R/mod_assumptions.R
@@ -10,20 +10,37 @@
 mod_assumptions_ui <- function(id) {
   ns <- NS(id)
   tagList(
-    HTML("<p>The code for this project is open-source, and is on <a href='https://github.com/ThomUK/DESPatientPathway' target='_blank'>GitHub.</a></p>"),
+    fluidRow(
+      column(
+        12,
+        h2("Source Code"),
+        HTML("<p>The code for this project is open-source, and is on <a href='https://github.com/ThomUK/DESPatientPathway' target='_blank'>GitHub.</a></p>")
+      )
+    ),
     fluidRow(
       column(
         width = 12,
+        h2("Notes"),
         p("This model is in development.  It is not yet ready to be used for planning.  Some of the issues to be resolved are detailed below."),
         HTML(
           "<ul>
               <li>Is the main queue appearing in the right place?  Is the typical real waiting list post referral and pre OP appointment, or is it post OP appt and pre-admission.</li>
-              <li>How to model the queue between OP appt and hospital admission?</li>
+              <li>How to visualise the queue between OP appt and hospital admission?</li>
               <li>The ward is modelled as a shared pre and post-operative ward, but the priority and behaviour of post-op vs. pre-op patients needs to be checked.</li>
               <li>Need to add an additional class of 'priority' patients (eg. cancer) in parallel to the standard patients. Define the queuing behaviour.</li>
               <li>The OP clinic and Theatre schedules are hard-coded as 8hrs/day, 7days/week.  This needs to be user-configurable.</li>
             </ul>"
         )
+      )
+    ),
+    fluidRow(
+      column(
+        12,
+        h2("Assumptions"),
+        h3("Probability distributions"),
+        p("Exponential distributions are used to model: Patient arrivals (referral times), ward bed length of stay, and theatre length of procedure."),
+        p("In the case of ward and theatre length of stay, this is for simplicity.  A log-normal or similar distribution may fit better, but would require tuning of 2 parameters to fit."),
+        p("There is no probability distribution modelled for OP clinic lengths.  These are assumed to be fixed and regular timings.")
       )
     ),
   )
