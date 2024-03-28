81e79e168fccad9ed2198e76a1c901a6e0893e10
Tom Smith <thomas.smith5@nuh.nhs.uk>
2024-03-28 11:04:39 +0000

    modify function to take df as input

--- R/simulate_worlds.R
+++ R/simulate_worlds.R
@@ -1,6 +1,6 @@
 #' Simulate multiple replications over multiple sets of initial conditions
 #'
-#' @param configs List. A list of lists containing multiple initial conditions
+#' @param configs Dataframe. A dataframe of initial conditions, each row being a simulation scenario
 #' @param n Integer. The number of replications to run for each initial condition
 #'
 #' @return List. A complex nested list structure to be analysed with the help
@@ -9,6 +9,8 @@
 #'
 simulate_worlds <- function(configs, n){
 
-  purrr::map(configs, \(x) run_multi_sim(x, n))
+  configs_list <- convert_to_nested_list(configs)
+
+  purrr::map(configs_list, \(x) run_multi_sim(x, n))
 
 }
